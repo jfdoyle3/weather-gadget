483494ee98c1d7dece0bc53cb68afe9253e45e38
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2025-07-23 22:23:28 -0400

    main.py: local was different from iot fix to be synced

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 '''
-  Weather Gadget
-  ----------------
-  
-    Free API Call:
-     https://api.open-meteo.com/v1/forecast?latitude=41.8501&longitude=-71.4662&current=temperature_2m,rain,apparent_temperature&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch
+    Weather Gadget
+   ----------------
+ Open Weather Current Temp - small respaonse set
+ https://api.open-meteo.com/v1/forecast?latitude=41.8501&longitude=-71.4662&current=temperature_2m&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch 
 
-'''
+ Test - Random Joke
+ https://official-joke-api.appspot.com/random_joke
+''' 
+
 # System Imports
 import os
 import sys
+import json
 
 # Add a directory to sys.path
-sys.path.append("/display/e-ink")
+sys.path.append("/display/lcd")
 sys.path.append("/network")
 sys.path.append("/resources")
+
+
 
 # Project Imports
 from wifiConnect import *
 from apiCalls import *
 from ledCodes import *
 from lcd import *
+from button import *
 
 def powerOff():
-    epd = EPD_2in13_V4_Landscape()
-    epd.init()
-    epd.Clear()
-    epd.delay_ms(2000)
-    epd.sleep()
+    lcdOff()
+    sys.exit()
+    
+
+def initApp():
+     print("wifi init")
+     lcdClear()
+     ip=wifiConnect()
 
 
 def main():
-    ip=wifiConnect()
-        
+    initApp()
+    '''        
     # No WiFi connection - Exit
     if not isinstance(ip, str):
         lcdNotConnected()
@@ -42,18 +51,31 @@ def main():
     lcdConnected()
     print(ip)
     ledOn()
+    '''    
     
     # res = requests.get(url='https://api.weather.gov/points/41.87092932,-71.42788283')
     # print(res.text)
-    apiURL='https://official-joke-api.appspot.com/random_joke'
+    apiURL='https://api.open-meteo.com/v1/forecast?latitude=41.8501&longitude=-71.4662&current=temperature_2m&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch '
     # Make the HTTP GET request
     try:
-        req=freeAPIRequest(apiURL)
+        # req=freeAPIRequest(apiURL)
+        with open('/files/f.json') as f:
+            data = json.load(f)
         
         # Access parsed data
-        print(req["type"])
-        print(req["setup"])
-        print(req["punchline"])
+        print(data)
+        print('>>------------------------->')
+        print('<-------------------------<<')
+        
+        temp="{:.0f}".format(data['current']['temperature_2m'])
+        unit=data['current_units']['temperature_2m']
+        
+        print(f"{temp}{unit}")
+        
+        #lcdText(f"{temp}{unit}",0,0)
+        lcdText("Hi",0,0)
+        
+        buttonPress()
         
     except Exception as e:
         print("Error:", e)
